09048ab55f7d3d191a670de5a2c61a605b54d643
Malchiel Rodrigues <malchielrodrigues@Malchiels-MBP.hsd1.fl.comcast.net>
2020-11-28 15:46:24 -0500

    Updated random

--- algorithms/specify_random.py
+++ algorithms/specify_random.py
@@ -8,7 +8,7 @@ from helpers.fiedler import fiedler as calc_fielder
 
 class SpecifyRandom(SpecifySmallStep):
 
-    def cut_edges(self, target, epsilon, bound = "two", allow_disconnected = False):
+    def cut_edges(self, target, bound, allow_disconnected):
         g = copy.deepcopy(self.graph)
         l = len(g)
         edges = [(u,v) for u in range(l) for v in range(u + 1, l) if g[u][v]]
@@ -22,20 +22,16 @@ class SpecifyRandom(SpecifySmallStep):
                 u,v = choice(edges_considering)
                 f, g_next = self.fiedler_without_edge(g,u,v)
                 # Invalid step
-                if (not allow_disconnected) and calc_fielder(g_next) < 0.0001:
+                if (not allow_disconnected) and self.fiedler_check(g_next) < 0.0001:
                     edges_considering.remove((u,v))
                     continue
                 # Terminal Cases
-                if bound == "two" and abs(f - target) <= epsilon:
-                    return f, g_next
+                if bound == "two" and abs(f - target) > abs(fiedler - target):
+                    edges_considering.remove((u,v))
+                    continue
                 elif bound == "one" and f < target:
                     edges_considering.remove((u,v))
                     continue
-                # elif f - target <= epsilon and f - target >= 0:
-                #     # This can cause a early/preemptive escape
-                #     return f, g_next
-                # Valid step
-                # if f - target >= 0.00001:
                 fiedler = f
                 g = g_next
                 edges.remove((u,v))
@@ -48,9 +44,9 @@ class SpecifyRandom(SpecifySmallStep):
 
         return fiedler, g
 
-    def create_graph(self, target, epsilon, bound = "two", allow_disconnected = False, runs = 10):
+    def create_graph(self, target, bound = "two", allow_disconnected = False, runs = 10):
         assert(bound == "two" or bound == "one")
-        graphs = [self.cut_edges(target, epsilon, bound, allow_disconnected) for i in range(runs)]
+        
+        graphs = [self.cut_edges(target, bound, allow_disconnected) for i in range(runs)]
         distance = [(abs(target - f),g) for (f, g) in graphs]
-        # print("random options", [round(o[0],3) for o in distance], [round(o[0],3) for o in graphs])
         return min(distance, key = lambda t: t[0])[1]
