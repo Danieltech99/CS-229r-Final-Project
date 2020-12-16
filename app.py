26a918ffd9389b778550e19994ab6ded2e506d6a
Malchiel Rodrigues <malchielrodrigues@Malchiels-MBP.hsd1.fl.comcast.net>
2020-12-15 22:07:42 -0500

    Line chart alg comparison

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from algorithms.specify_big_step import SpecifyBigStep
 from algorithms.specify_small_resistance import SpecifySmallLeverage
 from algorithms.specify_large_resistance import SpecifyLargeLeverage
 from algorithms.specify_small_gradient import SpecifySmallGradient
+from algorithms.specify_small_gradient_efficient import SpecifySmallGradientEfficient
 from algorithms.specify_large_gradient import SpecifyLargeGradient
 from algorithms.specify_random import SpecifyRandom
 from algorithms.specify_decision_tree import SpecifyDecisionTree
@@ -33,6 +34,7 @@ class TestFull(Test):
     name = "Full"
     target_connectivity = "N/A"
     bound = "N/A"
+    color = "slategray"
     def __init__(self, formation):
         # self.target_connectivity = target_connectivity
         # self.bound = bound
@@ -45,6 +47,7 @@ class TestFull(Test):
 
 class TestSpecifyDecisionTree(Test):
     name = "Decision Tree"
+    color = "black"
     def __init__(self, formation, fiedler_check, bound = "two", target_connectivity = 0):
         self.target_connectivity = target_connectivity
         self.bound = bound
@@ -55,7 +58,8 @@ class TestSpecifyDecisionTree(Test):
         return Graph(self.formation["nodes"], g)
 
 class TestSpecifySmallStep(Test):
-    name = "Small Step"
+    name = "Small Cut"
+    color = "tab:blue"
     def __init__(self, formation, fiedler_check, bound = "two"):
         # self.target_connectivity = target_connectivity
         self.bound = bound
@@ -66,7 +70,8 @@ class TestSpecifySmallStep(Test):
         return Graph(self.formation["nodes"], g)
 
 class TestSpecifyBigStep(Test):
-    name = "Big Step"
+    name = "Big Cut"
+    color = "tab:orange"
     def __init__(self, formation, fiedler_check, bound = "two"):
         # self.target_connectivity = target_connectivity
         self.bound = bound
@@ -78,6 +83,7 @@ class TestSpecifyBigStep(Test):
 
 class TestSpecifySmallLeverage(Test):
     name = "Small Leverage"
+    color = "limegreen"
     def __init__(self, formation, fiedler_check, bound = "two"):
         # self.target_connectivity = target_connectivity
         self.bound = bound
@@ -89,6 +95,7 @@ class TestSpecifySmallLeverage(Test):
 
 class TestSpecifyLargeLeverage(Test):
     name = "Big Leverage"
+    color = "forestgreen"
     def __init__(self, formation, fiedler_check, bound = "two"):
         # self.target_connectivity = target_connectivity
         self.bound = bound
@@ -101,6 +108,7 @@ class TestSpecifyLargeLeverage(Test):
 
 class TestSpecifySmallGradient(Test):
     name = "Small Gradient"
+    color = "tab:pink"
     def __init__(self, formation, fiedler_check, bound = "two"):
         # self.target_connectivity = target_connectivity
         self.bound = bound
@@ -110,8 +118,21 @@ class TestSpecifySmallGradient(Test):
         g = self.alg.create_graph(target_connectivity, self.bound)
         return Graph(self.formation["nodes"], g)
 
+class TestSpecifySmallGradientEfficient(Test):
+    color = "tab:brown"
+    name = "Small Gradient Efficient"
+    def __init__(self, formation, fiedler_check, bound = "two"):
+        # self.target_connectivity = target_connectivity
+        self.bound = bound
+        self.formation = formation
+        self.alg = SpecifySmallGradientEfficient(self.formation["full"], fiedler_check)
+    def create_graph(self, target_connectivity):
+        g = self.alg.create_graph(target_connectivity, self.bound)
+        return Graph(self.formation["nodes"], g)
+
 class TestSpecifyLargeGradient(Test):
     name = "Big Gradient"
+    color = "tab:purple"
     def __init__(self, formation, fiedler_check, bound = "two"):
         # self.target_connectivity = target_connectivity
         self.bound = bound
@@ -123,6 +144,7 @@ class TestSpecifyLargeGradient(Test):
 
 class TestSpecifyRandom(Test):
     name = "Random"
+    color = "tab:red"
     def __init__(self, formation, fiedler_check, bound = "two", runs = 10):
         self.bound = bound
         self.formation = formation
@@ -136,6 +158,40 @@ class TestSpecifyRandom(Test):
 
 
 
+# def plot_bar_alg(data, parameter_trials, forms, save_path = "compare_algorithms"):
+#     for formation in forms:
+#         labels = parameter_trials
+
+#         x = np.arange(len(labels))  # the label locations
+#         width = 0.10  # the width of the bars
+
+#         fig, ax = plt.subplots(figsize=(10,10))
+#         rects = []
+#         i = 0
+#         for name,bar_data in data.items():
+#             rects += [ax.bar(x + (width * i), bar_data[formation["name"]].values(), width, label=name, bottom=0)]
+#             i += 1
+
+#         # Add some text for labels, title and custom x-axis tick labels, etc.
+#         ax.set_ylabel('Achived Fiedler Value')
+#         ax.set_xlabel('Desired Fiedler Value')
+#         ax.set_title('Algorithm Comparision (Formation {})'.format(formation["name"]))
+#         ax.set_xticks(x + width / 2)
+#         ax.set_xticklabels(labels)
+#         # ax.legend()
+#         ax.legend(tuple([b[0] for b in rects]), tuple(data.keys()))
+#         ax.autoscale_view()
+
+#         fig.tight_layout()
+
+#         save_name = formation["name"]
+#         mkdir_p("figures/{}".format(save_path))
+#         if save_name: 
+#             plt.savefig("figures/{}/{}.png".format(save_path,save_name))
+#             plt.close()
+
+#     plt.show()
+
 def plot_alg(data, parameter_trials, forms, save_path = "compare_algorithms"):
     for formation in forms:
         labels = parameter_trials
@@ -146,18 +202,25 @@ def plot_alg(data, parameter_trials, forms, save_path = "compare_algorithms"):
         fig, ax = plt.subplots(figsize=(10,10))
         rects = []
         i = 0
-        for name,bar_data in data.items():
-            rects += [ax.bar(x + (width * i), bar_data[formation["name"]].values(), width, label=name, bottom=0)]
+        ax.plot(labels, labels,'--',label="Target",  color = 'silver', linewidth=4)
+        for key,bar_data in data.items():
+            # rects += [ax.bar(x + (width * i), bar_data[formation["name"]].values(), width, label=name, bottom=0)]
+            name,color = key
+            ls=['-','--','-.'][i%3]
+            if "Decision" in name: continue
+            if color is not None:
+                ax.plot(labels, bar_data[formation["name"]].values(),label=name,  linestyle=ls, color=color)
+            else: 
+                ax.plot(labels, bar_data[formation["name"]].values(),label=name,  linestyle=ls)
             i += 1
+        if "Decision" in name: ax.plot(labels, data["Decision Tree"][formation["name"]].values(),'-',label="Decision Tree",  color = 'black', linewidth=4, marker="*", linestyle = 'None')
 
         # Add some text for labels, title and custom x-axis tick labels, etc.
         ax.set_ylabel('Achived Fiedler Value')
         ax.set_xlabel('Desired Fiedler Value')
         ax.set_title('Algorithm Comparision (Formation {})'.format(formation["name"]))
-        ax.set_xticks(x + width / 2)
-        ax.set_xticklabels(labels)
-        # ax.legend()
-        ax.legend(tuple([b[0] for b in rects]), tuple(data.keys()))
+        ax.legend()
+        # ax.legend(tuple([b[0] for b in rects]), tuple(data.keys()))
         ax.autoscale_view()
 
         fig.tight_layout()
@@ -185,19 +248,20 @@ def simulate(forms, parameter_trials):
     dash = '-' * 40
     columns = ["Formation", "Algorithm", "Target λ", "Side", "Result λ", "Result ν", "Edges"]
     print(dash)
-    print('{:<24s}{:<20s}{:<8s}{:<8s}{:<10s}{:<10s}{:<20s}'.format(*columns))
+    print('{:<24s}{:<32s}{:<8s}{:<8s}{:<10s}{:<10s}{:<20s}'.format(*columns))
     print(dash)
 
     # Preload classes to allow decision tree to make only once
 
     results = {}
     alg_results = OrderedDict()
+    meta_stats = {}
     for formation in forms:
         results[formation["name"]] = {}
         # alg_results[formation["name"]] = {}
 
         test_types = [
-            TestSpecifyDecisionTree(formation, fiedler, bound="one", target_connectivity=min(parameter_trials)),
+            # TestSpecifyDecisionTree(formation, fiedler, bound="one", target_connectivity=min(parameter_trials)),
             TestSpecifyRandom(formation, fiedler, "one"),
             TestSpecifySmallStep(formation, fiedler, "one"),
             TestSpecifyBigStep(formation, fiedler, "one"),
@@ -211,7 +275,7 @@ def simulate(forms, parameter_trials):
         (test,graph) = (test, test.create_graph(None))
         nf = fiedler(graph.adj_matrix)
         results[formation["name"]]["true"] = nf
-        print('{:<24s}{:<20s}{:<8s}{:<8s}{:<10.4f}{:<10.4f}{:<20s}'.format(formation["name"], test.name, str(test.target_connectivity), test.bound, fiedler(graph.adj_matrix), normalized_fiedler(graph.adj_matrix), str(get_edges(graph.adj_matrix))))
+        print('{:<24s}{:<32s}{:<8s}{:<8s}{:<10.4f}{:<10.4f}{:<20s}'.format(formation["name"], test.name, str(test.target_connectivity), test.bound, fiedler(graph.adj_matrix), normalized_fiedler(graph.adj_matrix), str(get_edges(graph.adj_matrix))))
         print()
 
         trials = parameter_trials
@@ -225,16 +289,25 @@ def simulate(forms, parameter_trials):
                 # https://scientificallysound.org/2016/10/17/python-print3/
                 tnf = fiedler(graph.adj_matrix)
                 
-                test_n = test.name
+                test_n = (test.name,getattr(test, "color", None))
                 if test_n not in alg_results: alg_results[test_n] = {}
                 if formation["name"] not in alg_results[test_n]: alg_results[test_n][formation["name"]] = {}
                 if x not in alg_results[test_n][formation["name"]]: alg_results[test_n][formation["name"]][x] = {}
                 alg_results[test_n][formation["name"]][x] = tnf
                 
-                print('{:<24s}{:<20s}{:<8s}{:<8s}{:<10.4f}{:<10.4f}{:<20s}'.format(formation["name"], test.name, str(x), test.bound, fiedler(graph.adj_matrix), tnf, str(get_edges(graph.adj_matrix))))
+                print('{:<24s}{:<32s}{:<8s}{:<8s}{:<10.4f}{:<10.4f}{:<20s}'.format(formation["name"], test.name, str(x), test.bound, fiedler(graph.adj_matrix), tnf, str(get_edges(graph.adj_matrix))))
+                if test_n not in meta_stats:
+                    meta_stats[test_n] = {"edges" : 0}
+                meta_stats[test_n]["edges"] += (len(get_edges(formation["full"])) - len(get_edges(graph.adj_matrix)))
             print()
             
     plot_alg(alg_results,parameter_trials, forms)
+
+    print()
+    print("META STATS")
+    print("\t Edges:")
+    for (name,color), value in meta_stats.items():
+        print('\t\t{:<32s}{:<5d}'.format(name, value["edges"]))
 
 if __name__ == "__main__":
 
@@ -265,20 +338,21 @@ if __name__ == "__main__":
     # ... that takes in a formation and outputs a Graph object
     # Then add Test Class to Array
     # ... each test will be supplied a formation based on the args
+
+    
     # parameter_trials = [0.3, 0.35, 0.4, 0.45, 0.5, 0.6, 0.75, 1]
-    parameter_trials = [0.45, 0.5, 0.6, 0.75, 1]
-    # parameter_trials += [1.25, 1.5,1.75,2,2.5,3.2,4,4.5,6]
-    parameter_trials += [1.25, 1.5,1.75,2,2.5]
-    # parameter_trials = [0.5, 0.75]
+    # parameter_trials += [1.25, 1.5,1.75,2,2.5]
+
+
+    parameter_trials = [0.45, 0.6, 1, 1.25, 1.75, 2, 2.5]
     
     additional = []
-    for size in range(4,7):
-        for target in np.linspace(1,4,9):
-            additional.append({
-                "name": "n = {} and λ ≈ {}".format(size,round(target,2)),
-                "nodes": [None, None],
-                "full": random_graph(size, target)
-            })
+    for size in range(10,15,5):
+        additional.append({
+            "name": "n = {} and λ ≈ {}".format(size,round(10,2)),
+            "nodes": [None, None],
+            "full": random_graph(size, 10)
+        })
     forms = additional + forms
     simulate(forms, parameter_trials)
 
